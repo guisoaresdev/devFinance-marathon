6533785a891f5bec0a150bdf16e3fe777202809a
Guilherme Soares <soaresgui.dev@gmail.com>
2024-09-17 23:18:22 -0400

    feat: Adding GET, POST for transactions table

--- frontend/src/features/form/new_transaction/NewTransaction.tsx
+++ frontend/src/features/form/new_transaction/NewTransaction.tsx
@@ -3,10 +3,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleModal } from "../../../utils/redux/reducers/modalSlice";
 import { useAppSelector, useAppDispatch } from "../../../hooks/ReduxHooks";
 import { RootState } from "../../../utils/redux/store";
+import { Transaction } from "../../../interfaces/transaction.ts";
 import "./NewTransaction.css";
 
-function NewTransaction() {
-  const [inputs, setInputs] = useState({
+function NewTransaction({
+  onNewTransaction,
+}: {
+  onNewTransaction: (transaction: Transaction) => void;
+}) {
+  const [inputs, setInputs] = useState<Transaction>({
     description: "",
     amount: 0,
     date: new Date(),
@@ -20,11 +25,49 @@ function NewTransaction() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // Impede o reload da página
+    const transaction: Transaction = {
+      description: inputs.description,
+      amount: parseFloat(inputs.amount.toString()),
+      date: inputs.date,
+    };
+    const serializedBody = JSON.stringify(transaction);
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: serializedBody,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/transactions",
+        fetchOptions,
+      );
+
+      if (!response.ok) {
+        throw new Error("Erro ao salvar transação");
+      }
+      // Aqui você pode adicionar alguma ação após o sucesso, como limpar o formulário
+      console.log("Transação salva com sucesso");
+      const savedTransaction = await response.json();
+      onNewTransaction(savedTransaction.result); // Chama a função passada via props para atualizar o estado
+
+      console.log("Transação salva com sucesso");
+      toggle(); // Fecha o modal
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <div id="form">
         <h2>Nova Transação</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label className="sr-only" htmlFor="description">
               Descrição
@@ -67,7 +110,7 @@ function NewTransaction() {
             <a onClick={toggle} href="#" className="button cancel">
               Cancelar
             </a>
-            <button>Salvar</button>
+            <button type="submit">Salvar</button>
           </div>
         </form>
       </div>
